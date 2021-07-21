bd548038076fe8c0edb9a8147a42ecb8ea15b7de
Brent Abrahams <brent_abrahams@yahoo.com>
2021-07-22 00:15:17 +0800

    Updated for 2.3

--- package.js
+++ package.js
@@ -1,23 +1,23 @@
 Package.describe({
   name: 'constellation:theme-opaque',
-  version: '0.4.7',
+  version: '0.4.11',
   summary: 'Opaque theme plugin for Constellation',
   git: 'https://github.com/JackAdams/constellation-theme-opaque.git',
   documentation: 'README.md',
   debugOnly: true
 });
 
-Package.onUse(function(api) {
-  api.versionsFrom('1.0');
+Package.onUse(function (api) {
+  api.versionsFrom(['1.8.2', '2.3']);
 
-  api.use('constellation:console@1.4.7', 'client');
+  api.use('constellation:console@1.4.11', 'client');
 
-  api.addFiles('theme-opaque.css','client');
-  api.addFiles('theme-opaque.js','client');
+  api.addFiles('theme-opaque.css', 'client');
+  api.addFiles('theme-opaque.js', 'client');
   
   api.imply('constellation:console');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('tinytest');
 });
